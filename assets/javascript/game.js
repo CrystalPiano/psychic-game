6a0772b1b694bc3611cd29b4cccb192aae008f34
LucFII <LSFIIChi@Gmail.com>
2017-05-20 14:03:17 -0500

    updates to js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,45 +8,52 @@
 	var guessesLeft = 9;
 // The array of possible computer choices.
   var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-// The guessed letters
-	var guessedLetters = [];
+  // Computer Guess
+  var computerGuess = "";
 
 //============
 
-var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)]
-console.log(computerGuess);
+//var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)]
+//console.log(computerGuess);
 
-var reset = function() {
-  totalGuesses = 9;
+//function reset() {
+ // guessesLeft = 9;
+//}
+
+function gamereset() {
   guessesLeft = 9;
-  guessedLetters = [];
+  computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)]
+  console.log(computerGuess);  
+  //document.querySelector("#guessesLeft").innerHTML = "Guesses Left: " + guessesLeft;
 }
+
+gamereset();
 
 document.onkeyup = function(event) {
   var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
-  //document.querySelector("#guessLeft").innerHTML = "Guesses Left: " + guessesLeft;
+//document.querySelector("#guessesLeft").innerHTML = "Guesses Left: " + guessesLeft;
 
-  document.getElementById("#userGuess").innerHTML = "Guesses So Far: " + userGuess;
-
-        if (guessesLeft > 0){
-            if (userGuess === computerGuess){
-                wins++;
-                document.querySelector('#wins').innerHTML = "Wins: " + wins;
-                alert("You are a mind reader!");
-                reset();
-            }
-
-        else if(guessesLeft > 0){
-        	guessesLeft--;
-        	document.querySelector("#guessLeft").innerHTML = "Guesses Left: " + guessesLeft;
-        }
+if (guessesLeft > 0){
+      if (userGuess === computerGuess){
+        wins++;
+        document.querySelector('#wins').innerHTML = "Wins: " + wins;
+        alert("You are a mind reader!");
+        gamereset();
       }
 
-        else if(guessesLeft === 0){
-            losses++;
-            document.querySelector('#losses').innerHTML = "Losses: " + losses;
-            alert("Better sharpen your skillls!"); 
-            reset();
-        }  
+        else {
+          guessesLeft--;
+          document.querySelector("#guessesLeft").innerHTML = "Guesses Left: " + guessesLeft;
+      }
+    }
+
+    else {
+      losses++;
+      document.querySelector('#losses').innerHTML = "Losses: " + losses;
+      //document.querySelector("#guessesLeft").innerHTML = "Guesses Left: " + guessesLeft;
+      alert("Better sharpen your skills!"); 
+      gamereset();
+    }  
+  
 };
